0a16d05bc9755c736941195f18dcbbe389c2e4ce
Jeff Barbee <jeffbarbee@ascend-rw01.ten.osc.edu>
2025-07-01 12:53:05 -0400

    Branch assignment

--- RegressionPython.py
+++ RegressionPython.py
@@ -1,86 +1,43 @@
-
-import sys
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-
-if len(sys.argv) != 4:
-    print("Usage: python linear_regression_python.py <filename> <x_column> <y_column>")
-    sys.exit(1)
-
-filename = sys.argv[1]
-x_col = sys.argv[2]
-y_col = sys.argv[3]
-
-data = pd.read_csv(filename)
-model = LinearRegression()
-model.fit(data[[x_col]], data[[y_col]])
-
-plt.scatter(data[[x_col]], data[[y_col]], color='red')
-plt.plot(data[[x_col]], model.predict(data[[x_col]]), color='blue')
-plt.title(f'{y_col} vs {x_col}')
-plt.xlabel(x_col)
-plt.ylabel(y_col)
-plt.savefig("linear_regression_python_output.png")
-plt.show()
-
 #!/usr/bin/env python
 # coding: utf-8
 
 # This notebook demonstrates a simple linear regression analysis using [Python/R] to model Salary based on Years of Experience.
 
-# In[7]:
+## Updated Assignment
+ 
+
+# In[2]:
 
 
-import pandas as pd
-dataset = pd.read_csv("regression_data.csv")
-
-
-# In[10]:
-
-
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
+from sklearn.metrics import mean_squared_error
 
 
-# In[11]:
+# In[3]:
 
 
-plt.scatter(dataset["YearsExperience"], dataset["Salary"], color="red")
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([2.1, 4.3, 6.1, 8.0, 10.1])
 
+# Linear regression
+slope, intercept, r_value, p_value, std_err = linregress(x, y)
+y_pred = slope * x + intercept
+mse = mean_squared_error(y, y_pred)
 
-# In[18]:
-
-
-from sklearn.linear_model import LinearRegression
-
-
-# In[19]:
-
-
-model = LinearRegression()
-
-
-# In[20]:
-
-
-model.fit(dataset[["YearsExperience"]], dataset[["Salary"]])
-
-
-# In[22]:
-
-
-plt.scatter(dataset["YearsExperience"], dataset["Salary"], color="red")
-plt.plot(dataset["YearsExperience"], model.predict(dataset[["YearsExperience"]]), color="blue")
-plt.title("Salary vs Experience")
-plt.xlabel("Years of Experience")
-plt.ylabel("Salary")
+# Plot
+plt.plot(x, y_pred, 'r-', label='Fitted Line')
+plt.text(1.5, max(y) - 1,
+         f"y = {slope:.2f}x + {intercept:.2f}\n"
+         f"r = {r_value:.2f}\nMSE = {mse:.2f}",
+         fontsize=12)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Regression")
+plt.legend()
+plt.savefig("regression_plot_python.png")
 plt.show()
-
-
-# In[23]:
-
-
-model.score(dataset[["YearsExperience"]], dataset[["Salary"]])  # R-squared
 
 
 # In[ ]:
